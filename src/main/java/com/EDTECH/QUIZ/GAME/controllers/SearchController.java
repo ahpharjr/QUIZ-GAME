2a8b071ea4a53cea06fbdbd85f1bb26a4e7b3976
ahpharjr <6531503132@lamduan.mfu.ac.th>
2025-01-15 00:28:45 +0700

    add basic search function

--- src/main/java/com/EDTECH/QUIZ/GAME/controllers/SearchController.java
+++ src/main/java/com/EDTECH/QUIZ/GAME/controllers/SearchController.java
@@ -1,13 +1,43 @@
 package com.EDTECH.QUIZ.GAME.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.EDTECH.QUIZ.GAME.models.Flashcard;
+import com.EDTECH.QUIZ.GAME.repositories.FlashcardRepository;
 
 @Controller
 public class SearchController {
+
+    @Autowired
+    private FlashcardRepository flashcardRepository;
     
     @GetMapping("/search-card")
-    public String search() {
-        return "search_card";
+    public String search(@RequestParam(name= "keyword", required = false) 
+                        String keyword, Model model) {
+        
+        List<Flashcard> searchResults = List.of(); // Default empty list
+        if (keyword != null && !keyword.isBlank()) {
+            searchResults = flashcardRepository.searchByKeyword(keyword);
+        }
+        model.addAttribute("searchResults", searchResults);
+        model.addAttribute("keyword", keyword); // Retain the keyword in the input field
+         return "search_card";
     }
+
+    @GetMapping("/search-suggestions")
+    @ResponseBody
+    public List<String> getSuggestions(@RequestParam String keyword) {
+        return flashcardRepository.searchByKeyword(keyword).stream()
+                .map(Flashcard::getKeyword)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/EDTECH/QUIZ/GAME/repositories/FlashcardRepository.java
+++ src/main/java/com/EDTECH/QUIZ/GAME/repositories/FlashcardRepository.java
@@ -2,7 +2,9 @@ package com.EDTECH.QUIZ.GAME.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.EDTECH.QUIZ.GAME.models.Flashcard;
@@ -10,4 +12,8 @@ import com.EDTECH.QUIZ.GAME.models.Flashcard;
 @Repository
 public interface FlashcardRepository extends JpaRepository<Flashcard, Long>{
     List<Flashcard> findAllByTopicTopicId(Long topicId);
+
+    //method for keyword search
+    @Query("SELECT f FROM Flashcard f WHERE LOWER(f.keyword) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Flashcard> searchByKeyword(@Param("keyword") String keyword);
 }
