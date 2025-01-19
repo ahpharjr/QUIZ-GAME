5df4787ad7067fbb9cefc9ee13162aa490929f0c
ahpharjr <6531503132@lamduan.mfu.ac.th>
2025-01-19 19:29:04 +0700

    commit left images

--- src/main/java/com/EDTECH/QUIZ/GAME/controllers/QuizSetController.java
+++ src/main/java/com/EDTECH/QUIZ/GAME/controllers/QuizSetController.java
@@ -1,13 +1,22 @@
 package com.EDTECH.QUIZ.GAME.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.EDTECH.QUIZ.GAME.repositories.TopicRepository;
 
 @Controller
 public class QuizSetController {
+    @Autowired
+    private TopicRepository topicRepository;
     
-    @GetMapping("/quizset")
-    public String quizSet() {
+    @GetMapping("{phaseId}/quizset")
+    public String quizSet(@PathVariable Long phaseId, Model model) {
+
+        model.addAttribute("topics", topicRepository.findAllByPhasePhaseId(phaseId));
         return "quiz_set";
     }
 }
