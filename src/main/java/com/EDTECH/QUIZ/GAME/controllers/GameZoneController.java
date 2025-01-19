8925f8f5c55696dc434c8d4a921aff00f16f67d0
ahpharjr <6531503132@lamduan.mfu.ac.th>
2025-01-19 13:25:27 +0700

    display pahse in game zone

--- src/main/java/com/EDTECH/QUIZ/GAME/controllers/GameZoneController.java
+++ src/main/java/com/EDTECH/QUIZ/GAME/controllers/GameZoneController.java
@@ -1,13 +1,22 @@
 package com.EDTECH.QUIZ.GAME.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.EDTECH.QUIZ.GAME.repositories.PhaseRepository;
 
 @Controller
 public class GameZoneController {
+    @Autowired
+    private PhaseRepository phaseRepository;
     
     @GetMapping("/gamezone")
-    public String gameZone() {
+    public String gameZone(Model model) {
+        var phases = phaseRepository.findAll();
+        model.addAttribute("phases", phases);
+
         return "game_zone";
     }
 }
