39a2e07579ce7be21f46abec544300011990a41e
6531503194 <6531503194@lamduan.mfu.ac.th>
2025-02-04 21:17:27 +0700

    import

--- src/main/java/com/EDTECH/QUIZ/GAME/Config/SecurityConfig.java
+++ src/main/java/com/EDTECH/QUIZ/GAME/Config/SecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.EDTECH.QUIZ.GAME.sevices.CustomOAuth2User;
@@ -105,6 +107,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
 
     return http.build();
 }
+
 
     // Authentication manager bean
     // @Bean
